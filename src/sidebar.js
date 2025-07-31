f12b677423aeacb09c2cb24ce427789014482dbc
Seçgin ŞANLI <secginsanli@gmail.com>
2025-07-31 23:35:31 +0300

    darl ve ligt theme

--- src/sidebar.js
+++ src/sidebar.js
@@ -8,29 +8,47 @@ const DATA_KEY = 'bs.sidebar';
 const EVENT_KEY = `.${DATA_KEY}`;
 const DATA_API_KEY = '.data-api'
 
+const EVENT_LOAD_DATA_API = `load${EVENT_KEY}${DATA_API_KEY}`;
 const EVENT_CLICK_DATA_API = `click${EVENT_KEY}${DATA_API_KEY}`;
 
 const CLASS_NAME_CLOSE = 'close';
 const CLASS_NAME_SUB_MENU_SHOW = 'show';
 const CLASS_NAME_DROPDOWN_ROTATE = 'rotate';
 
-const SELECTOR_SIDEBAR = '.sidebar';
+const SELECTOR_SIDEBAR = '[data-bs-sidebar]';
 const SELECTOR_TOGGLE = '.sidebar-toggle';
 const SELECTOR_DROPDOWN = '.sidebar-dropdown';
 const SELECTOR_SUB_MENU = '.sidebar-sub-nav';
 
-export default class Sidebar extends BaseComponent {
+const STORAGE_KEY = 's-sidebar-state';
+
+class Sidebar extends BaseComponent {
 
     constructor(element, config) {
         super(element, config);
+        this.initSidebarState();
     }
 
     static get NAME() {
         return NAME
     }
 
+    initSidebarState() {
+        const savedState = localStorage.getItem(STORAGE_KEY);
+        if (savedState === 'closed') {
+            this._element.classList.add(CLASS_NAME_CLOSE);
+        }
+        
+        setTimeout(() => {
+            this._element.classList.add('ready');
+        }, 50);
+    }
+
     sidebarToggle() {
         this._element.classList.toggle(CLASS_NAME_CLOSE);
+
+        const isClosed = this._element.classList.contains(CLASS_NAME_CLOSE);
+        localStorage.setItem(STORAGE_KEY, isClosed ? 'closed' : 'open');
     }
 
     dropdownToggle(dropdownElement) {
@@ -41,10 +59,16 @@ export default class Sidebar extends BaseComponent {
     }
 
     static create(element, config = {}) {
-        const sidebarEl = SelectorEngine.parents(element,SELECTOR_SIDEBAR)[0];
+        const sidebarEl = SelectorEngine.parents(element, SELECTOR_SIDEBAR)[0];
         return Sidebar.getOrCreateInstance(sidebarEl, config);
     }
 }
+
+EventHandler.on(window, EVENT_LOAD_DATA_API, () => {
+    for (const element of SelectorEngine.find(SELECTOR_SIDEBAR)) {
+        Sidebar.getOrCreateInstance(element)
+    }
+});
 
 EventHandler.on(document, EVENT_CLICK_DATA_API, SELECTOR_TOGGLE, function (event) {
     event.preventDefault();
@@ -55,3 +79,5 @@ EventHandler.on(document, EVENT_CLICK_DATA_API, SELECTOR_DROPDOWN, function (eve
     event.preventDefault();
     Sidebar.create(this).dropdownToggle(this);
 });
+
+export default Sidebar;
